e9332eb51d13d52cb6adfa8deefe3128bff452b1
Leila <146056583+KerimovaLeyla@users.noreply.github.com>
2024-02-10 22:11:09 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// const picture = document.getElementById("picture")
+// const name = document.getElementById("name")
+// const title = document.getElementById("title")
+// const price = document.getElementById("price")
+// const myForm = document.getElementById("myForm")
+
+
+// function postProduct(e) {
+//     e.preventDefault();
+//     axios
+//       .post(`https://65680f6b9927836bd9740785.mockapi.io/swp102/product`, {
+//         image:picture.value,
+//         name: name.value,
+//         price: price.value
+//       })
+//       .then((res) => {
+//         getproducts();
+//         postform.reset();
+//       });
+//   }
+//   myForm.addEventListener("submit", postProduct);
+
+
+
